7d43bf909b078399243831fa1a7d580d63cbc67a
Le Thanh Binh <thanhbinhle41@gmail.com>
2023-05-01 13:22:53 +0700

    Set up notification expo with fire base

--- App.js
+++ App.js
@@ -1,26 +1,83 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Alert } from "react-native";
 
-import HomeScreen from './src/screens/HomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
-import TabNavigator from './src/navigation/TabNavigator';
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+import TabNavigator from "./src/navigation/TabNavigator";
+
+import messaging from '@react-native-firebase/messaging';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   const CustomDefaultTheme = {
     colors: {
-      background: '#ffffff',
-      text: '#333333'
+      background: "#ffffff",
+      text: "#333333",
+    },
+  };
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      // return fcm token for the device
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    } else {
+      console.log("Failed to get device token");
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            "Notification caused app to open from quit state:",
+            remoteMessage.notification
+          );
+          // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+        }
+      });
+
+      messaging().onNotificationOpenedApp(async (remoteMessage) => {
+        console.log(
+          'Notification caused app to open from background state:',
+          remoteMessage.notification,
+        );
+      });
+
+      messaging().setBackgroundMessageHandler(async remoteMessage => {
+        console.log('Message handled in the background!', remoteMessage);
+      });
+
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      });
+  
+      return unsubscribe;
+
+
+  }, []);
 
   return (
-    <NavigationContainer theme={CustomDefaultTheme}> 
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+    <NavigationContainer theme={CustomDefaultTheme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="TabNavigator" component={TabNavigator} />
